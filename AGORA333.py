41a60a43e7ad21de082ec16a732a648abfc3de16
GroundswellDanceCompany <amandakaniewska46@gmail.com>
2025-05-02 13:20:26 +0100

    Update AGORA333.py

--- AGORA333.py
+++ AGORA333.py
@@ -469,7 +469,7 @@ just human voices and emotional clarity.
 
                 
                    
-                
+                if not just_comments: 
                     # Full Agora Mode
                     with st.spinner("Gathering the emotional field..."):
                         summary = generate_ai_summary(selected_headline, emotion_groups)
